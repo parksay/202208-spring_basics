ca1a329d978cfe93d20f842da1ab46a84d83c59b
parksay_desktop <hellobigworld.parksay@gmail.com>
2022-08-31 21:51:19 +0900

    220831 - discount and order

---
+++ src/main/java/hello/core/order/Order.java
@@ -0,0 +1,67 @@
+package hello.core.order;
+
+public class Order {
+    // 주문 엔터티 생선
+    // 주문 정보가 들어 있음
+    // 누가 무슨 상품을 샀고, 가격은 얼만에 얼마 만큼 할인해 줄지
+    
+    private Long memberId;
+    private String itemName;
+    private int itemPrice;
+    private int discountPrice;
+
+    public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
+        this.memberId = memberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.discountPrice = discountPrice;
+
+    }
+
+    public int calculatePrice() {
+
+        return itemPrice - discountPrice;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public void setDiscountPrice(int discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "memberId=" + memberId +
+                ", itemName='" + itemName + '\'' +
+                ", itemPrice=" + itemPrice +
+                ", discountPrice=" + discountPrice +
+                '}';
+    }
+}
---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,25 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+    // 주문을 생성하는 역할의 구현체
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // 주문을 하면 주문한 사람 정보를 불러 옴.
+    // 주문자 정보를 바탕으로 VIP인지 일반인지 판단해서 얼마나 할인해 줄지 결정.
+    // 주문자 정보, 주문 상품 정보, 원래 가격, 할인된 가격을 바탕으로 주문 정보를 되돌려줌.
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
