ed0a96de2415ea1bd962fc3892fd000e32f9d591
parksay_desktop <hellobigworld.parksay@gmail.com>
2022-09-04 12:42:34 +0900

    220904 - add comments on order app

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -15,10 +15,14 @@ public class OrderServiceTest {
     @Test
     void createOrder() {
         long memberId = 1L;
+        // 회원 정보 하나를 생성
         Member member = new Member(memberId, "memberA", Grade.VIP);
+        // 그 회원 정보를 데이터베이스에 추가
         memberService.join(member);
 
+        // 회원, 상품, 가격 -> 누가 무엇을 얼마에 샀는지 정보를 가지고 주문 정보 생성
         Order order = orderService.createOrder(memberId, "itemA", 10000);
+        // 주문한 결과 가격을 알아서 잘 계산해 줬는지 확인
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
 }
